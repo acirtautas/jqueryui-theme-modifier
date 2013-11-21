4ce8abebea081403aaf63fe534de526c87e165a2
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2013-11-22 00:43:00 +0200

    Basic working theme modifyer

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-jqueryui-theme-mod
-==================
+jQuery UI theme modifier for Composer
+==========================================
 
-jQuery UI theme modifyer
+Simple tool to modify jQuery UI theme constants in Composer.
+
+Installation / Usage
+--------------------
+
+1. Download the [`composer.phar`](https://getcomposer.org/composer.phar) executable or use the installer.
+
+    ``` sh
+    $ curl -sS https://getcomposer.org/installer | php
+    ```
+
+2. Create a composer.json defining your dependencies.
+
+    ``` json
+    {
+        "require": {
+            "acirtautas/jqueryui-theme-modifier": "dev-master"
+        },
+        "config": {
+            "jqueryui-theme-dir": "components/jquery-ui/themes/base",
+            "jqueryui-theme-modify": {
+                "cornerRadius":"6px"
+            }
+        },
+        "scripts": {
+            "post-update-cmd": [
+                "modifier::modify"
+            ],
+            "post-install-cmd": [
+                "modifier::modify"
+            ]
+        }
+    }
+    ```
+
+3. Run Composer: `php composer.phar install`
---
+++ modifier.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * jQuery UI theme modifier for Composer.
+ *
+ * @author Alfonsas Cirtautas
+ */
+
+use Composer\Script\Event;
+
+/**
+ * Class modifier.
+ */
+class modifier {
+
+    /**
+     * Composer hook to execute.
+     *
+     * @param Event $event
+     */
+    public static function modify(Event $event)
+    {
+        $composer = $event->getComposer();
+        $config   = $composer->getConfig();
+        $io       = $event->getIO();
+
+        $modify   = $config->has('jqueryui-theme-modify') ? $config->get('jqueryui-theme-modify') : array();
+        $dir      = $config->has('jqueryui-theme-dir') ? $config->get('jqueryui-theme-dir') : 'components/jquery-ui/themes/base';
+
+        if(!is_array($modify) || !count($modify)) {
+            return;
+        }
+
+        if (!is_dir($dir)) {
+            $io->write('Can not find jQuery UI theme dir '.$dir);
+            return;
+        }
+
+        $io->write('Modifying jQuery UI theme ['.$dir.']');
+
+        $pattern = '/(.[^\s\*:]+)\/\*\{([a-z]+)\}\*\//i';
+
+        foreach (new DirectoryIterator($dir) as $fileInfo) {
+            if($fileInfo->isDot() || $fileInfo->isDir()) continue;
+
+            $filename = $fileInfo->getFilename();;
+
+            $css  = file_get_contents($dir.'/'.$filename);
+
+            preg_match_all($pattern, $css, $matches);
+
+            $modified = 0;
+
+            if(count($matches)) {
+                foreach($matches[0] as $i => $data) {
+                    $value = $matches[1][$i];
+                    $name  = $matches[2][$i];
+
+                    if(isset($modify[$name])){
+                        $replace = ' '.$modify[$name];
+                        $css = str_replace($data, $replace.'/*{'.$name.'}*/', $css, $count);
+
+                        $modified += $count;
+                    }
+                }
+            }
+
+            if($modified) {
+                $io->write('    '.$filename.' '.str_repeat('.', $modified));
+                file_put_contents($dir.'/'.$filename, $css);
+            }
+
+        }
+    }
+} 
